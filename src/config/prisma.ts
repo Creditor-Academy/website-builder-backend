bacda33f87e4ded755f37fcf1b25d424153dea53
Vasu Bhatia <vasu.bhatia@lmsathena.com>
2026-06-30 11:23:01 +0530

    chore: disable NODE_TLS_REJECT_UNAUTHORIZED to allow self-signed database certificates

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -5,6 +5,9 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config({ quiet: true });
 
+// Force Node.js to accept self-signed certificates from AWS/Supabase globally
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const { Pool } = pg;
 const pool = new Pool({
   connectionString: process.env.POSTGRESQL_URL,
